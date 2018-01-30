ba6ac7ce88bacdd4bac48f8ef41b99eb690ee76d
jheilhet <shirley.mramirez@yahoo.com>
2018-01-30 10:31:56 -0700

    added comments in bamazonCustomer file and validation check if the item id inputted by the customer exists in the database or not

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,15 @@
+// dependencies needed to run the application
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
 const chalk = require("chalk");
 
+// constant declaration for chalk library and console.log
 const error = chalk.bold.red;
 const warning = chalk.keyword("orange");
 const log = console.log;
 
+// this line will read and connect what's in the bamazon_db database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -15,6 +18,8 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+// read what is in the database through connection.query "SELECT", 
+// then output it in the table using CLI-table packages
 connection.connect(function(err) {
     if (err) throw err;
     connection.query("SELECT item_id, product_name, price, product_sales FROM products", function(err, res) {
@@ -28,6 +33,8 @@ connection.connect(function(err) {
                 colAligns: ['center']
             }
         });
+
+        // this part of the code will loop through each result data and push in the table 
         for (var i = 0; i < res.length; i++) {
             table.push([res[i].item_id, res[i].product_name, res[i].price, res[i].product_sales]);
         }
@@ -40,8 +47,9 @@ connection.connect(function(err) {
     });
 });
 
+// ask customer for an input(purchase item and quantity)
+// validate the input if it is a number or not
 function startOrder() {
-
     inquirer.prompt([{
         name: "itemId",
         type: "input",
@@ -65,44 +73,57 @@ function startOrder() {
             }
         }
     }]).then(function(answer) {
+        // store in a var the user's answer for the quantity
         var stockQuantity = answer.quantity;
         var queryStr = "SELECT * FROM products WHERE ?";
 
+        // then read the database through the item-id 
         connection.query(queryStr, { item_id: answer.itemId }, function(err, res) {
             var productData = res[0];
-            if (stockQuantity <= productData.stock_quantity) {
+            // check if the item ID exists or not in the database
+            if (res.length === 0) {
                 log();
-                log("We have enough stocks for you, placing your order now ...");
+                log(error("Item not found, enter a valid" + chalk.green(" Item ID")));
                 log();
-
-                //this query here is for the stock_quantity update in the mysql database 
-                var updateQueryStr = "UPDATE products SET stock_quantity = " + (productData.stock_quantity - stockQuantity) + " WHERE ?";
-                connection.query(updateQueryStr, { item_id: productData.item_id, }, function(err, res) {
-                    if (err) throw err;
-                    log(chalk.green("Your order has been placed."));
-                    log();
-                    log(chalk.magenta("\n----------------------------------------\n"));
-                    log(chalk.yellow(stockQuantity) + " items purchased");
-                    log(chalk.cyan(productData.product_name) + " which cost" + " $" + chalk.yellow(productData.price) + " each");
-                    log("Your " + chalk.cyan("Total Cost ") + "is $" + chalk.yellow(productData.price * stockQuantity));
-                    log("Thank you for shopping with us!");
-                    log(chalk.magenta("\n---------------------------------------\n"));
-
-                    // this query here is for the product sale update in the mysql database 
-                    // which is the requirement under supervisor view
-                    var updateQueryStrSales = "UPDATE products SET product_sales = " + (productData.price * stockQuantity) + " WHERE ?";
-                    connection.query(updateQueryStrSales, { item_id: productData.item_id, }, function(err, res) {
-                        if (err) throw err;
-                        process.exit(1);
-                    })
-                })
-
+                startOrder();
             } else {
-                console.log();
-                log(error("Sorry, we have insufficient quantity of ") + chalk.green(productData.product_name));
-                log(error("Please modify your order."));
-                log(chalk.magenta("\n-----------------------------------------\n"));
-                process.exit(1);
+                // check if there's enough stocks in the database
+                if (stockQuantity <= productData.stock_quantity) {
+                    log();
+                    log("We have enough stocks for you, placing your order now ...");
+                    log();
+
+                    // this query here is for the stock_quantity update in the mysql database 
+                    // which is the only requirement when customer is purchasing an item (first part)
+                    var updateQueryStr = "UPDATE products SET stock_quantity = " + (productData.stock_quantity - stockQuantity) + " WHERE ?";
+                    connection.query(updateQueryStr, { item_id: productData.item_id, }, function(err, res) {
+                        if (err) throw err;
+                        log(chalk.green("Your order has been placed."));
+                        log();
+                        log(chalk.magenta("\n----------------------------------------\n"));
+                        log(chalk.yellow(stockQuantity) + " items purchased");
+                        log(chalk.cyan(productData.product_name) + " which cost" + " $" + chalk.yellow(productData.price) + " each");
+                        log("Your " + chalk.cyan("Total Cost ") + "is $" + chalk.yellow(productData.price * stockQuantity));
+                        log("Thank you for shopping with us!");
+                        log(chalk.magenta("\n---------------------------------------\n"));
+
+                        // this query here is for the product sale update in the mysql database 
+                        // which is the requirement under supervisor view(third part)
+                        var updateQueryStrSales = "UPDATE products SET product_sales = " + (productData.price * stockQuantity) + " WHERE ?";
+                        connection.query(updateQueryStrSales, { item_id: productData.item_id, }, function(err, res) {
+                            if (err) throw err;
+                            process.exit(1);
+                        })
+                    })
+
+                } else {
+                    // when there's not enough stock it will show a log saying ... 
+                    console.log();
+                    log(error("Sorry, we have insufficient quantity of ") + chalk.green(productData.product_name));
+                    log(error("Please modify your order."));
+                    log(chalk.magenta("\n-----------------------------------------\n"));
+                    process.exit(1);
+                }
             }
         });
     })
